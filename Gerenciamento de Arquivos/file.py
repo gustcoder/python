40e61f7a03c9f3cd8cb666df19daf06cfd8cab62
gustcoder <gustavofaramos@gmail.com>
2021-12-22 22:04:31 -0300

    Manipulando arquivos

--- Gerenciamento de Arquivos/file.py
+++ Gerenciamento de Arquivos/file.py
@@ -5,10 +5,20 @@ BASE_PATH = os.path.abspath(__file__)
 
 dir = os.path.dirname(BASE_PATH)
 
-file = open(dir + '/_file_test.dat', 'w')
+# file = open(dir + '/_file_test.dat', 'w')
+
+# file.write('##########################\n')
+# file.write('School of Net\n')
+# file.write('##########################\n')
+
+# file.close
+
+file = open(dir + '/_file_test.dat', 'a')
 
 file.write('##########################\n')
 file.write('School of Net\n')
 file.write('##########################\n')
+
+file.writelines(['PFA\n', 'RabbitMQ\n', 'Docker\n'])
 
 file.close
